49e5cf8dc47d0e7ac7b680d3810b0bcc88723b01
SouBarGit <116821959+SouBarGit@users.noreply.github.com>
2022-11-04 08:29:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,5 @@ streamlit.dataframe(my_fruit_list)
 
 #lets put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list))
+#display the table on the page
+streamlit.dataframe(my_fruit_list)
